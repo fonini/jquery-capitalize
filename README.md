fe925a0940ae89bfb68e0515c5023e1c23771351
Jonnas Fonini <contato@fonini.net>
2014-07-24 16:37:49 -0300

    Release 1.0.0

---
+++ README.md
@@ -0,0 +1,43 @@
+jQuery Capitalize
+================================
+
+The jQuery Capitalize Plugin provides capitalization for your inputs fields (or any other element) for proper names, places etc.
+
+## Getting Started
+
+You have three choices to get the plugin:
+
+### Download latest release
+
+Download the [latest release](https://github.com/fonini/jquery-capitalize/releases)
+
+### Install it with Bower
+
+`bower install jquery-capitalize`
+
+### Download the latest changes
+
+ 1. Fork or clone this repo
+ 2. Run `npm install` to install the build tools
+ 3. Run `gulp`
+ 4. The generated file will be at the dist folder
+
+## Using the plugin
+
+Include jQuery and the plugin on a page. Apply the plugin to the elements you want.
+
+```html
+<input type="text" id="name" />
+
+<script src="jquery.js"></script>
+<script src="jquery.capitalize.min.js"></script>
+<script>
+$(document).ready(function(){
+	$('#name').capitalize();
+});
+</script>
+```
+
+## License
+
+Copyright (c) 2014 Jonnas Fonini. Licensed under the MIT license.
---
+++ src/jquery.capitalize.js
@@ -0,0 +1,79 @@
+;
+(function($, window, document, undefined) {
+  var pluginName = 'capitalize';
+  var ignoredKeys = [8, 13, 16, 17, 18, 37, 39];
+
+  var NN_POINT = /\./g;
+  var NN_POINT_SPACE = '. ';
+  var NN_SPACE = ' ';
+  var NN_REGEX_MULTIPLE_SPACES = /\s+/g;
+  var NN_REGEX_ROMAN_NUMERAL = /^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
+
+  function Plugin(element) {
+    this.element = element;
+    this._name = pluginName;
+    this.init();
+    this.ignoredKeys = ignoredKeys;
+  }
+
+  Plugin.prototype = {
+    init: function() {
+
+      if ($(this.element).is('input')) {
+        var that = this;
+
+        $(this.element).on('keydown', function(e) {
+          if ($.inArray(e.keyCode, that.ignoredKeys) === -1) {
+            $(this).val(that.capitalize($(this).val()));
+          }
+        });
+
+        $(this.element).on('blur', function(e) {
+          $(this).val(that.capitalize($(this).val()));
+        });
+      } else {
+        $(this.element).text(this.capitalize($(this.element).text()));
+      }
+    },
+    capitalize: function(str) {
+      str = str.replace(NN_POINT, NN_POINT_SPACE);
+
+      str = str.replace(NN_REGEX_MULTIPLE_SPACES, NN_SPACE);
+
+      str = str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      });
+
+      var parts = str.split(NN_SPACE);
+
+      var exceptions = [
+        'de', 'di', 'do', 'da', 'dos', 'das', 'dello', 'della',
+        'dalla', 'dal', 'del', 'e', 'em', 'na', 'no', 'nas', 'nos', 'van', 'von',
+        'y'
+      ];
+
+      for (var i = 0; i < parts.length; ++i) {
+        for (var j = 0; j < exceptions.length; j++) {
+          if (parts[i].toLowerCase() == exceptions[j].toLowerCase()) {
+            parts[i] = exceptions[j];
+          }
+        }
+
+        if (parts[i].toUpperCase().match(NN_REGEX_ROMAN_NUMERAL)) {
+          parts[i] = parts[i].toUpperCase();
+        }
+      }
+
+      return parts.join(NN_SPACE);
+    }
+  };
+
+  $.fn[pluginName] = function() {
+    return this.each(function() {
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' + pluginName, new Plugin(this));
+      }
+    });
+  };
+
+})(jQuery, window, document);
